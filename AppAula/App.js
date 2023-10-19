bc7b07a71956056f50fcf2a3ec9dab86a7fb7459
Iago Souza <iagosouza@MacBook-Air-de-Iago.local>
2023-10-19 13:55:47 -0300

    att

---
+++ AppAula/App.js
@@ -0,0 +1,24 @@
+import 'react-native-gesture-handler'
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack'
+import UsersPage from './Pages/usersPage';
+import { UsersProvider } from './Context/UsersContext';
+import PrincLogin from './Pages/PaginaPrinc/PrincLogin';
+import { Inscrever, Login } from './Pages/PaginaPrinc/Login';
+
+export default function App() {
+  const Stack = createStackNavigator();
+  return (
+    <NavigationContainer>
+      <UsersProvider>
+        <Stack.Navigator>
+          <Stack.Screen name='Users' options={{headerShown:false}} component={UsersPage} />
+          <Stack.Screen name="Welcome" options={{headerShown:false}} component={PrincLogin} />
+          <Stack.Screen name="Login" component={Login} />
+          <Stack.Screen name="SignUp" component={Inscrever} />
+        </Stack.Navigator>
+      </UsersProvider>
+    </NavigationContainer>
+  );
+}
+
